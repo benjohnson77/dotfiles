b7c665bfbce6b33dbe9419dcb6e72eb70e669d10
Ben Johnson <benjaminrjohnson99@gmail.com>
2022-08-20 14:16:12 -0500

    made sure zshrc was linked

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ ln -s ~/.dotfiles/.config/nvim/autoload/plug.vim ~/.vim/autoload/
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
 ln -s .bash_profile ~/.bash_profile
-
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
