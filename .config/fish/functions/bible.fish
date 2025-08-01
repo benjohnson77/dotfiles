570e6a1974f28f4eab6801615904f516d871c501
Ben Johnson <benjaminrjohnson99@gmail.com>
2025-07-31 19:05:03 -0500

    major clean up

--- .config/fish/functions/bible.fish
+++
@@ -1,3 +0,0 @@
-function bible
-	~/Sites/apps/BibleApp/bin/bible --lang en $argv
-end
--- install.sh
+++
@@ -1,5 +0,0 @@
-ln -s ~/.dotfiles/.config ~/.config
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
-ln -s .bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
