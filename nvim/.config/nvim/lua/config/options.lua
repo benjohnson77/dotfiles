15aaf3b931b6bc9e373967fac5d7376e70351510
Ben Johnson <benjaminrjohnson99@gmail.com>
2025-10-12 15:40:13 -0500

    tunning the setup script

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Show hidden files in file explorers
+vim.g.netrw_hide = 0  -- Show all files in netrw
+vim.g.netrw_list_hide = ""  -- Don't hide any files
